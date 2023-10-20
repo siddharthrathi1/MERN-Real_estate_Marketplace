bfac2f37f5e72b69b193c4991f7b532e960253d5
Siddharth Rathi <f20190730@goa.bits-pilani.ac.in>
2023-10-21 00:43:09 +0530

    Created Header component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,10 +4,12 @@ import SignIn from './Pages/Signin';
 import SignUp from './Pages/SignUp';
 import Profile from './Pages/Profile';
 import About from './Pages/About';
+import Header from './components/Header';
 
 
 export default function App() {
   return <BrowserRouter>
+  <Header/>
   <Routes>
     < Route path='/' element={<Home/>}/>
     < Route path='/sign-in' element={<SignIn/>}/>
